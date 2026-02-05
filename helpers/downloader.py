905a74e700ad3478e7becc8d2e5d0225562cb52a
kncabatuan <kncabatuan@yahoo.com>
2026-02-05 16:23:29 +0800

    WIP validation for Youtube URL

--- helpers/downloader.py
+++ helpers/downloader.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-#Class for Youtube URL
+
+# Class for Youtube URL
 class Download:
-    def __init__(self, url: str, type: str, mode: str):
+    def __init__(self, url: str, type: str, mode: str) -> None:
         self.url = url
         self.type = type
         self.mode = mode
@@ -10,13 +11,15 @@ class Download:
     @property
     def url(self) -> str:
         return self._url
-    
+
     @url.setter
-    def url(self, url: str):
-        #Insert validation here for URL
+    def url(self, url: str) -> None:
+        pattern = r"^(?:https?://)?(?:www\.|m\.)?(?:youtube\.com|youtu\.be)/
+        # Insert validation here for URL
         self._url = url
 
-#Class for file-name
+
+# Class for file-name
 class Save_Directory:
-    def __init__(self, filepath: Path):
+    def __init__(self, filepath: Path) -> None:
         self.filepath = filepath
