8d71829b42f195c3634803821c52fd8ab5655e18
kncabatuan <kncabatuan@yahoo.com>
2026-02-13 09:36:05 +0800

    Refactored single download code from main to its own function

--- ez_youtube.py
+++ ez_youtube.py
@@ -17,57 +17,13 @@ def main() -> None:
     while True:
         match menu.get_user_choice():
             case "1":
-                download_mode = "single"
+                single_download()
             case "2":
                 print("Go to batch download")
             case "3":
                 print("Go to playlist download")
             case "exit":
                 menu.exit_program()
-
-        url, file_type = get_user_inputs()
-        menu.print_checking()
-
-        if download_object := downloader.create_download_obj(
-            url, file_type, download_mode
-        ):
-            menu.print_obj_success(download_object.title)
-            time.sleep(DELAY)
-        else:
-            menu.print_obj_fail()
-            time.sleep(DELAY)
-            continue
-
-        filepath = menu.get_filepath()
-        if filepath == "exit":
-            menu.exit_program()
-        else:
-            assert isinstance(filepath, Path)
-            download_object.save_path(filepath)
-
-        menu.print_checking()
-        time.sleep(DELAY)
-
-        assert download_object.title is not None
-        decision = menu.get_final_decision(
-            download_mode, download_object.title, file_type, download_object.filepath
-        )
-        if decision == "y":
-            try:
-                download_object.download_vid()
-                menu.print_dl_success()
-            except yt_dlp.utils.ExtractorError:
-                menu.print_exception("ExtractorError")
-                menu.print_dl_fail()
-            except (yt_dlp.utils.DownloadError, yt_dlp.utils.PostProcessingError):
-                menu.print_exception("DownloadError")
-                menu.print_dl_fail()
-        elif decision == "n":
-            menu.print_dl_fail()
-        else:
-            menu.exit_program()
-
-        time.sleep(DELAY)
 
 
 def get_user_inputs() -> tuple:
@@ -87,6 +43,55 @@ def get_user_inputs() -> tuple:
         menu.exit_program()
 
     return url, file_type
+
+
+def single_download():
+    download_mode = "single"
+    url, file_type = get_user_inputs()
+
+    menu.print_checking()
+
+    if download_object := downloader.create_download_obj(
+        url, file_type, download_mode
+    ):
+        menu.print_obj_success(download_object.title)
+        time.sleep(DELAY)
+    else:
+        menu.print_obj_fail()
+        time.sleep(DELAY)
+        return
+
+    filepath = menu.get_filepath()
+    if filepath == "exit":
+        menu.exit_program()
+    else:
+        assert isinstance(filepath, Path)
+        download_object.save_path(filepath)
+
+    menu.print_checking()
+    time.sleep(DELAY)
+
+    assert download_object.title is not None
+    decision = menu.get_final_decision(
+        download_mode, download_object.title, file_type, download_object.filepath
+    )
+    if decision == "y":
+        try:
+            download_object.download_vid()
+            menu.print_dl_success()
+        except yt_dlp.utils.ExtractorError:
+            menu.print_exception("ExtractorError")
+            menu.print_dl_fail()
+        except (yt_dlp.utils.DownloadError, yt_dlp.utils.PostProcessingError):
+            menu.print_exception("DownloadError")
+            menu.print_dl_fail()
+    elif decision == "n":
+        menu.print_dl_fail()
+    else:
+        menu.exit_program()
+
+    time.sleep(DELAY)
+    return
 
 
 if __name__ == "__main__":
