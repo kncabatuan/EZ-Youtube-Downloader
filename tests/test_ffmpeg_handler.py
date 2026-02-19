d9b3e321d23d0d33275610137830679ea5ad73f5
kncabatuan <kncabatuan@yahoo.com>
2026-02-19 19:17:35 +0800

    Updated due to rename of file

--- tests/test_ffmpeg_handler.py
+++ tests/test_ffmpeg_handler.py
@@ -1,6 +1,6 @@
-from helpers import system_check
+from helpers import ffmpeg_handler
 from unittest.mock import patch
 
 def test_dependency_checking():
     with patch("shutil.which", return_value = None):
-        assert system_check.check_dependency() == False
+        assert ffmpeg_handler.check_ffmpeg() == False   
